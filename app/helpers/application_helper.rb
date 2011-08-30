57d494891ec41bb47fe7ebdd61467c09cac07aa8
James Dunwoody <jamesdunwoody@gmail.com>
2011-08-30 17:43:28 +1000

    Added Users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,13 @@ module ApplicationHelper
     # RAILS_DEFAULT_LOGGER.info ("in ApplicationHelper title")
     base_title = "Ruby on rails tutorial sample app"
     if @title.nil?
-      base_title
+    base_title
     else
       "#{base_title} | #{@title}"
     end
+  end
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
 end
