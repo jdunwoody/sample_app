141122dc99e8706a24d55f03a43f40be65241fa0
James Dunwoody <jamesdunwoody@gmail.com>
2011-08-30 16:51:32 +1000

    Added styles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
-    it "should be successful" do
+     it "should be successful" do
+       get 'home'
+       response.should be_success
+     end
+
+    it "should have right title" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title")
+#, :content => "Home")
     end
   end
 
